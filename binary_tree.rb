380f718a276e8cc5b273dc6744a6553f2706f493
Alan Vardy <alanvardy@gmail.com>
2018-09-03 13:42:01 -0600

    finished breadth_first_search method

--- binary_tree.rb
+++ binary_tree.rb
@@ -1,4 +1,5 @@
 class BinaryTree
+
   def initialize
     @head = nil
   end
@@ -21,6 +22,26 @@ class BinaryTree
   def build_tree(arr)
     arr.each {|node| add_node(node)}
   end
+
+  def breadth_first_search(value)
+    def bfs(value, node = @head)
+      puts "Node: #{node.value}, left child: #{node.left_child.value unless node.left_child.nil?}, right child: #{node.right_child.value unless node.right_child.nil?}"
+      if node.value == value
+        @found = true
+        return node
+      else
+        @queue.push(node.left_child) unless node.left_child.nil?
+        @queue.push(node.right_child) unless node.right_child.nil?
+      end
+    end
+    return nil if @head == nil
+    @queue = [@head]
+    @found = false
+    while !@queue.empty? && @found == false
+      answer = bfs(value, @queue.slice!(0))
+    end
+    return answer
+  end
 end
 
 class Node
@@ -35,3 +56,4 @@ end
 
 binarytree = BinaryTree.new
 binarytree.build_tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
+puts binarytree.breadth_first_search(9).inspect
